b54d3044badc6388faf2919d440065e4500fb802
Ashish Nandan Singh <ashish.singh@finly.io>
2020-01-05 20:48:29 +0530

    added new post template

---
+++ v1.2/gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Personal Blog`,
+    author: `Ashish Nandan Singh`,
+    description: `Personal blogging site dedicated to all things development`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/ashishnandansin`,
+      },
+      {
+        name: `Github`,
+        url: `https://github.com/ashishcodes4`,
+      },
+      {
+        name: `LinkedIn`,
+        url: `https://www.linkedin.com/in/ashish-nandan-singh/`,
+      },
+    ],
+  },
+}
---
+++ v1.2/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    <Styled.a href="http://twitter.com/ashishnandansin">Ashish Nandan Singh</Styled.a>
+    {` `}
+    <br />
+    Dreamer by day, programmer by night.
+  </Fragment>
+)
